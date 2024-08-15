a6750e6907a332dd972b9adecb02dd3a8d8662ff
aarysinhdodiya <aarysinhdodiya05@gmail.com>
2024-08-15 09:25:29 +0530

    Add files via upload

---
+++ Data Types.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	char Class='X';
+	int Rank=8;
+	float Percentage=89.57;
+	
+	printf("In my %cth class\n My Rank was %d\n I got %.2f %%",Class,Rank,Percentage);
+	return 0;
+}
