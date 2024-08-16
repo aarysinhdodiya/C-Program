b4835a66ae8e96bcf91d96772e58ea374bd4549b
aarysinhdodiya <aarysinhdodiya05@gmail.com>
2024-08-16 18:11:40 +0530

    Add files via upload

---
+++ Memory Size.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	int Number;
+	float Decimal;
+	char Character;
+	double LongDecimal;
+	
+	printf("%1u\n",sizeof(Number));
+	printf("%1u\n",sizeof(Decimal));
+	printf("%1u\n",sizeof(Character));
+	printf("%1u\n",sizeof(LongDecimal));
+	
+}
